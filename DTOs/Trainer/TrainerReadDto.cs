e68ff0c1e90159aabcc740b362186b886d89c69d
global133 <sergej.chuev.98@mail.ru>
2025-06-11 08:46:51 +0300

    added new field in model

--- DTOs/Trainer/TrainerReadDto.cs
+++ DTOs/Trainer/TrainerReadDto.cs
@@ -2,12 +2,17 @@
 {
     public class TrainerReadDto
     {
+        public int Id { get; set; }
         public string Login { get; set; } = null!;
         public string FullName { get; set; } = null!;
         public DateTime DateOfBirth { get; set; }
+        public string? Description { get; set; }
         public string Phone { get; set; } = null!;
         public string Gender { get; set; } = null!;
         public string Status { get; set; } = null!;
         public string Specialization { get; set; } = null!;
+        public int Age =>
+        DateTime.Now.Year - DateOfBirth.Year -
+        (DateTime.Now.DayOfYear < DateOfBirth.DayOfYear ? 1 : 0);
     }
 }
