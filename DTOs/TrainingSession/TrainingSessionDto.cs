624a8ec124696435a083ad1adb3f1538b4a22dbf
global133 <sergej.chuev.98@mail.ru>
2025-07-03 17:18:54 +0300

    added new entity

---
+++ DTOs/TrainingSession/TrainingSessionDto.cs
@@ -0,0 +1,12 @@
+﻿using MyGymProject.Server.DTOs.Client;
+
+namespace MyGymProject.Server.DTOs.TrainingSession
+{
+    public class TrainingSessionDto
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public List<ClientReadDto> BookedClients { get; set; } = new();
+    }
+}
